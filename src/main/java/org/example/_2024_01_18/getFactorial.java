566b86e17c1441e1c34753660823f40657f9db53
RinasAlex <alexandrarinas777@gmail.com>
2024-01-25 20:01:29 +0100

    homework 4

--- src/main/java/org/example/_2024_01_18/getFactorial.java
+++ src/main/java/org/example/_2024_01_18/getFactorial.java
@@ -5,11 +5,11 @@ public class getFactorial {
         int a = 4;
         int result = 1;
         for (int i = 1; i <= a; i++) {
+
             result = result * i;
+
+
         }
         System.out.println("Factorial " + a + " = " + result);
-
-
-
     }
 }
