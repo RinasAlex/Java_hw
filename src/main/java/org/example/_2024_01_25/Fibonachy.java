7d1bb244d97b8de756cc5c143321c2ffdc348626
RinasAlex <alexandrarinas777@gmail.com>
2024-01-28 16:59:51 +0100

    homework 4

--- src/main/java/org/example/_2024_01_25/Fibonachy.java
+++ src/main/java/org/example/_2024_01_25/Fibonachy.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
 public class Fibonachy {
     public static void main(String[] args) {
         int[] array = new int[]{};
-        System.out.println(Arrays.toString(getFibonachy(array)));
-    }
-
-    public static int[] getFibonachy(int[] array) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int firstNumber = 0;
-        int secondNumber = 1;
-        int[] number = new int[n];
+
+        System.out.println(Arrays.toString(getFibonachy(n, array)));
+    }
+
+    public static int[] getFibonachy(int n, int[] array) {
+
+        int[] massiv = new int[n];
+        massiv[0] = 0;
+        massiv[1] = 1;
 
         for (int i = 2; i < n; i++) {
-            number[i] = firstNumber + secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = number[i];
+            massiv[i] = massiv[i-1] + massiv[i-2];
         }
 
-        return number;
+
+        return massiv;
     }
 }
